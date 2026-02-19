d26c049d643491f3c3f0c5719bd49de3b7ff75c5
Erik Moqvist <erik.moqvist@gmail.com>
2026-02-19 10:26:36 +0100

    Fix iOS.

--- contrib/mbedtls/library/bignum.c
+++ contrib/mbedtls/library/bignum.c
@@ -1403,7 +1403,7 @@ int mbedtls_mpi_sub_int(mbedtls_mpi *X, const mbedtls_mpi *A, mbedtls_mpi_sint b
  * \param b             A scalar to multiply.
  */
 static
-#if defined(__APPLE__) && defined(__arm__)
+#if defined(__APPLE__) && (defined(__arm__) || defined(__aarch64__))
 /*
  * Apple LLVM version 4.2 (clang-425.0.24) (based on LLVM 3.2svn)
  * appears to need this to prevent bad ARM code generation at -O3.
